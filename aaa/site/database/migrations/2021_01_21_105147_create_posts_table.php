6d015391d500924cd446553dc82e49ac006041f4
Aleksandr <aleksstanov1984@gmail.com>
2021-01-21 13:04:00 +0200

    Add CRUD

---
+++ aaa/site/database/migrations/2021_01_21_105147_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('userId');
+            $table->string('title');
+            $table->string('text');
+            $table->boolean('published')->default(0);
+            $table->timestamp('creat_at')->nullable();
+            $table->timestamp('update_at')->nullable();
+
+            $table->foreign('userId')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
