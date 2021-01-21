e00dde73309052a73f0a3436a5296c517e86da69
Aleksandr <aleksstanov1984@gmail.com>
2021-01-21 14:49:42 +0200

    Add Controllers

--- aaa/site/app/Models/Models/NewUser.php
+++ aaa/site/app/Models/Models/NewUser.php
@@ -5,19 +5,18 @@ namespace App\Models\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class NewUser extends Model
 {
-    //use HasFactory;
+    use HasFactory;
+
     const CREATED_AT = 'createdAt';
     const UPDATED_AT = 'updatedAt';
 
-    public $table = 'posts';
+    public $table = 'new_users';
 
     protected $fillable = [
-        'title', 'text', 'userId'
+        'name', 'surname', 'email', 'phone_number', 'address'
     ];
 
-    public function user(){
-        return $this->belongsTo(User::class, 'userId');
-    }
+
 }
--- aaa/site/app/Models/User.php
+++ aaa/site/app/Models/User.php
@@ -1,19 +1,43 @@
 <?php
 
-namespace App\Models\Models;
+namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
-   // use HasFactory;
-    const CREATED_AT = 'createdAt';
-    const UPDATED_AT = 'updatedAt';
+    use HasFactory, Notifiable;
 
-    public $table = 'users';
-
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-        'name', 'surname', 'email', 'phone_number', 'address', 'birthday'
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
 }
